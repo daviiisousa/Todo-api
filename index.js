f2c1186688741df6ad6a392fec9372b8c1d27c73
daviiisousa <sousadavi248@gmail.com>
2025-06-09 19:38:26 -0300

    sobe

--- index.js
+++ index.js
@@ -53,18 +53,60 @@ app.post("/todos", async (req, res) => {
     if (descricao.length > 100) {
       return res.status(400).send("a descricao tem mais de 100 caracteres");
     }
-    const todo = await pool.query(`
+    const todo = await pool.query(
+      `
         INSERT INTO todos (titulo, descricao)
         VALUES (
-            '${titulo}',
-            '${descricao}'
+            $1,
+            $2
         )    
         RETURNING *
-    `);
+    `,
+      [titulo, descricao]
+    );
 
     return res.status(201).send(todo.rows[0]);
   } catch (error) {
     console.error(error);
+    return res.status(500).send("erro no servidor: ", error);
+  }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const todo = await pool.query(`SELECT * FROM todos WHERE id = $1`, [id]);
+
+    if (todo.rows.length === 0) {
+      return res.status(404).send("Tarefa não encontrada");
+    }
+
+    await pool.query(`DELETE FROM todos WHERE id = $1`, [id]);
+
+    return res.status(200).send("Tarefa deletada");
+  } catch (error) {
+    console.error("Erro ao deletar tarefa:", error);
+    return res.status(500).send("Erro no servidor");
+  }
+});
+
+app.put("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { descricao } = req.body;
+  try {
+    if (!descricao) res.status(400).send("preencha os dados corretamente");
+
+    const todo = await pool.query(
+      `
+        UPDATE todos SET descricao = $1 WHERE id = $2
+
+        RETURNING *
+      `,
+      [descricao, id]
+    );
+
+    return res.status(201).send(todo.rows[0]);
+  } catch (error) {
     return res.status(500).send("erro no servidor: ", error);
   }
 });
